23b953c4f8975ba86664148ea783ad6820d50f2c
kdpenner <kdpenner@users.noreply.github.com>
2015-08-24 12:21:25 +0200

    getbunch checks if obsid already downloaded

--- storage/getbunch.py
+++ storage/getbunch.py
@@ -14,11 +14,21 @@ for row in reader:
 
 f.close()
 
-obsids = obsids[3:5]
+obsids = obsids[1:]
 
 pool_name = raw_input('Enter a pool name:')
 
+query = ProductStorage([PoolManager.getPool(pool_name)]).select(herschel.ia.pal.query.MetaQuery(herschel.ia.obs.ObservationContext, "p", "1"))
+
+listofobsids = []
+
+for result in query:
+  listofobsids.append(result.product.obsid)
+
 for obsid in obsids:
-  print 'Getting '+obsid+' and storing in '+pool_name
-  obs = getObservation(obsid, instrument = 'PACS', useHsa = True)
-  saveObservation(obs, poolName = pool_name, saveCalTree = True)
+  if long(obsid) in listofobsids:
+    print 'Obsid already exists in pool. Skipping download.'
+  else:
+    print 'Getting '+obsid+' and storing in '+pool_name
+    obs = getObservation(obsid, instrument = 'PACS', useHsa = True)
+    saveObservation(obs, poolName = pool_name, saveCalTree = True)
