7e94020ad1ae5c44ce77ecd008ac8621027c2400
kdpenner <kdpenner@users.noreply.github.com>
2016-05-02 15:16:33 +0200

    Removed obsID restriction in deactivatemask

--- reduce/deactivatemask.py
+++ reduce/deactivatemask.py
@@ -12,17 +12,14 @@ def deactivatemask(poolname, camera):
   obses = help.getallobscontexts(poolname)
 
   for obs in obses:
-    if obs.obsid in [1342186610, 1342186611, 1342186625, 1342186626, \
-    1342186627, 1342186628, 1342187058, 1342187059, 1342187060, 1342187061, \
-    1342188073, 1342188074]:
 
-      level1 = PacsContext(obs.level1)
-      frames = level1.averaged.getCamera(camera).product
+    level1 = PacsContext(obs.level1)
+    frames = level1.averaged.getCamera(camera).product
       
-      frames.removeMask('badprobs')
+    frames.removeMask('badprobs')
       
-      obs = updatePacsObservation(obs, 1.0, frames)
+    obs = updatePacsObservation(obs, 1.0, frames)
       
-      saveObservation(obs, poolName = poolname)
+    saveObservation(obs, poolName = poolname)
 
   return
